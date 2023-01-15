5c4bc3187edab98d164386381b3612e110d446a1
kenggbua <kevin.wiltschnig@hotmail.com>
2023-01-15 19:21:14 +0100

    dasd

--- Prototype 1/review/screens/d12245cc-1680-458d-89dd-4f0d7fb22724.js
+++ Prototype 1/review/screens/d12245cc-1680-458d-89dd-4f0d7fb22724.js
@@ -2,7 +2,7 @@ var content='<div class="ui-page" deviceName="iphone13promax" deviceType="mobile
     <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devMobile devIOS iphone-device canvas firer commentable non-processed" alignment="left" name="Template 1" width="428" height="926">\
     <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
     <div id="alignmentBox">\
-      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1673185352893.css" />\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1673806382675.css" />\
       <div class="freeLayout">\
       </div>\
 \
@@ -12,34 +12,9 @@ var content='<div class="ui-page" deviceName="iphone13promax" deviceType="mobile
     <div id="s-d12245cc-1680-458d-89dd-4f0d7fb22724" class="screen growth-vertical devMobile devIOS iphone-device canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="Screen 1" width="428" height="926">\
     <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
     <div id="alignmentBox">\
-      <link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1673185352893.css" />\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1673806382675.css" />\
       <div class="freeLayout">\
-      <div id="s-Group_19" class="group firer ie-background commentable non-processed" customid="Dialog Field" datasizewidth="0.0px" datasizeheight="0.0px" >\
-        <div id="s-Input_17" class="text firer focusin focusout commentable non-processed" customid="Dialog input"  datasizewidth="400.0px" datasizeheight="40.0px" dataX="14.0" dataY="264.0" ><div class="backgroundLayer">\
-          <div class="colorLayer"></div>\
-          <div class="imageLayer"></div>\
-        </div><div class="borderLayer"><div class="paddingLayer"><div class="content"><div class="valign"><input type="text"  value="" maxlength="100"  tabindex="-1" placeholder="Label"/></div></div>  </div></div></div>\
-        <div id="s-Subtraction_5" class="path firer click commentable hidden non-processed" customid="Cancel icon"   datasizewidth="18.0px" datasizeheight="18.0px" dataX="385.0" dataY="275.0"  >\
-          <div class="borderLayer">\
-          	<div class="imageViewport">\
-            	<?xml version="1.0" encoding="UTF-8"?>\
-            	<svg xmlns="http://www.w3.org/2000/svg" width="18.00006103515625" height="17.99981689453125" viewBox="384.9999575569427 275.0000900163445 18.00006103515625 17.99981689453125" preserveAspectRatio="none">\
-            	  <g>\
-            	    <defs>\
-            	      <path id="s-Subtraction_5-d1224" d="M391.2579399028205 280.8407529084603 C391.35813920692084 280.8407529084603 391.4584134170548 280.8787596734752 391.5348425398687 280.9548602039484 L393.99712870330467 283.4116905748904 L396.4649579132172 280.9548602039484 C396.54131212999744 280.8788342452837 396.6414864654202 280.8408274802687 396.74166080084285 280.8408274802687 C396.8418101675876 280.8408274802687 396.94195953433245 280.8788342452837 397.0183387197906 280.9548602039484 C397.17052281111694 281.10636526134965 397.17107212202893 281.3537199498155 397.0183387197906 281.505771867145 L394.549960198966 283.9631490980152 L397.0122713310798 286.4210731888137 C397.1644304537283 286.57257824621496 397.1650047333182 286.81993293468076 397.0116970514899 286.971437992082 C396.9353178660318 287.04746395074676 396.83516849928697 287.08547071576174 396.7350191325421 287.08547071576174 C396.63484479711946 287.08547071576174 396.5346704616968 287.04746395074676 396.45831624491655 286.971437992082 L393.99657939239256 284.51406076121174 L391.5348425398687 286.9648259584037 C391.45853826044396 287.04077734526004 391.3583389563436 287.07865982392775 391.25821455827645 287.07865982392775 C391.15789041078705 287.07865982392775 391.05761620065294 287.04064063027806 390.98146173329536 286.9648259584037 C390.82872833105694 286.8127740410743 390.82872833105694 286.56596621253675 390.98146173329536 286.4139142952072 L393.44374789673134 283.9626022380869 L390.98088745370524 281.50522500721667 C390.82872833105694 281.35262622995896 390.82872833105694 281.10580597278664 390.98201104420735 280.95431334402014 C391.05804066817586 280.8786229584932 391.15794034814246 280.8407529084603 391.2579399028205 280.8407529084603 Z M394.000025069932 275.0000900163445 C389.0505090339156 275.0000900163445 384.9779179316927 279.067870306902 385.0000901175987 284.0 C384.977992837726 288.91517703532145 389.0225441147547 292.97784915700356 393.95975059260485 292.99982298321237 C393.97318374127315 292.999885126386 393.9865919212637 292.99990998365547 394.0000001012542 292.99990998365547 C398.94949116859283 292.99990998365547 403.022057302138 288.93211726446316 402.9999100849097 284.0 C403.02198239610453 279.08482296467855 398.97745608775375 275.02216327163114 394.0402496099036 275.00017701678763 C394.02684142991296 275.00011487361394 394.0134332499226 275.0000900163445 394.000025069932 275.0000900163445 Z "></path>\
-            	    </defs>\
-            	    <g style="mix-blend-mode:normal">\
-            	      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s-Subtraction_5-d1224" fill="#8E8E93" fill-opacity="1.0"></use>\
-            	    </g>\
-            	  </g>\
-            	</svg>\
-\
-            </div>\
-          </div>\
-        </div>\
-      </div>\
-\
-      <div id="s-Button_1" class="button multiline manualfit firer click commentable non-processed" customid="Button Filled"   datasizewidth="145.0px" datasizeheight="50.0px" dataX="214.0" dataY="443.9" >\
+      <div id="s-Paragraph_1" class="richtext manualfit firer ie-background commentable non-processed" customid="Smart Lights app"   datasizewidth="183.0px" datasizeheight="53.0px" dataX="122.5" dataY="245.0" >\
         <div class="backgroundLayer">\
           <div class="colorLayer"></div>\
           <div class="imageLayer"></div>\
@@ -48,16 +23,63 @@ var content='<div class="ui-page" deviceName="iphone13promax" deviceType="mobile
           <div class="paddingLayer">\
             <div class="content">\
               <div class="valign">\
-                <span id="rtr-s-Button_1_0">Button</span>\
+                <span id="rtr-s-Paragraph_1_0">Smart Lights app</span>\
               </div>\
             </div>\
           </div>\
         </div>\
       </div>\
-      <div id="s-Input_8" class="inputIOS checkbox firer commentable non-processed unchecked" customid="Toggle"  datasizewidth="51.0px" datasizeheight="31.0px" dataX="277.0" dataY="629.0"      tabindex="-1">\
+      <div id="s-Input_1" class="text firer commentable non-processed" customid="Input 1"  datasizewidth="300.0px" datasizeheight="45.0px" dataX="64.0" dataY="440.5" ><div class="backgroundLayer">\
+        <div class="colorLayer"></div>\
+        <div class="imageLayer"></div>\
+      </div><div class="borderLayer"><div class="paddingLayer"><div class="content"><div class="valign"><input type="text"  value="" maxlength="100"  tabindex="-1" placeholder=""/></div></div>  </div></div></div>\
+      <div id="s-Input_2" class="text firer commentable non-processed" customid="Input 2"  datasizewidth="300.0px" datasizeheight="45.0px" dataX="64.0" dataY="544.4" ><div class="backgroundLayer">\
+        <div class="colorLayer"></div>\
+        <div class="imageLayer"></div>\
+      </div><div class="borderLayer"><div class="paddingLayer"><div class="content"><div class="valign"><input type="text"  value="" maxlength="100"  tabindex="-1" placeholder=""/></div></div>  </div></div></div>\
+      <div id="s-Paragraph_2" class="richtext autofit firer ie-background commentable non-processed" customid="Login"   datasizewidth="39.1px" datasizeheight="18.0px" dataX="64.0" dataY="406.0" >\
         <div class="backgroundLayer">\
           <div class="colorLayer"></div>\
           <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_2_0">Login</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Paragraph_3" class="richtext autofit firer ie-background commentable non-processed" customid="Password"   datasizewidth="70.3px" datasizeheight="36.0px" dataX="64.0" dataY="510.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_3_0">Password<br /><br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Button_1" class="button multiline manualfit firer click commentable non-processed" customid="Button Filled"   datasizewidth="145.0px" datasizeheight="50.0px" dataX="219.0" dataY="633.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Button_1_0">Login</span>\
+              </div>\
+            </div>\
+          </div>\
         </div>\
       </div>\
       </div>\
